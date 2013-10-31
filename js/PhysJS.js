f977112eb2da1f09ac632e66991955d7802f197d
edom18 <edo.m18@gmail.com>
2013-10-31 13:38:18 +0900

    added a new function.

--- js/PhysJS.js
+++ js/PhysJS.js
@@ -14,8 +14,27 @@ window.PhysJS = window.PhysJS || {};
 
     PhysJS.lengthSqr = function (vec) {
         var result;
+
         result = (vec.x * vec.x);
         result = (result + (vec.y * vec.y));
+        result = (result + (vec.z * vec.z));
+
+        return result;
+    };
+
+    //衝突点4つから最大の面積を求める？
+    PhysJS.calcArea4Points = function (p0, p1, p2, p3) {
+        var areaSqrA = PhysJS.lengthSqr(
+                vec3.cross(vec3.sub(p0, p1), vec3.sub(p2, p3))
+            );
+        var areaSqrB = PhysJS.lengthSqr(
+                vec3.cross(vec3.sub(p0, p2), vec3.sub(p1, p3))
+            );
+        var areaSqrC = PhysJS.lengthSqr(
+                vec3.cross(vec3.sub(p0, p3), vec3.sub(p1, p2))
+            );
+
+        return Math.max(Math.max(areaSqrA, areaSqrB), areaSqrC);
     };
 
 }(window, document, PhysJS));
